ba9afa7ad1caf7b4f08d2b2694ad6c6de8718f26
gilvamgomes <ggomes.fhb@gmail.com>
2023-05-10 19:19:56 -0300

    Aula2 + exerc

---
+++ aula2-entrada-e-saida.py
@@ -0,0 +1,39 @@
+"""
+nome = input('Escreva seu nome: ')
+idade = input('Escreva sua idade: ')
+altura = input('Escreva sua altura: ')
+
+print(nome, 'tem', idade, "anos e", altura, 'de altura')""
+"""
+'''
+num1 = 12
+num2 = 12
+num3 = 2
+resultado = num1 + (num2/num3)
+
+print(resultado)
+'''
+
+'''
+Faça um formulário que pergunte o nome, cpf, endereço, idade, altura
+e telefone e imprima issoe m um relatório organizado
+'''
+
+print("Formulário do Exercício 2\n")
+
+nome = input('Escreva seu nome: ')
+cpf = input('Escreva seu CPF: ')
+endereço = input('Escreva seu endereço: ')
+idade = input('Escreva sua idade: ')
+altura = input('Escreva sua altura: ')
+telefone = input('Escreva seu telefone: ')
+
+
+print("\nRelatório:\nNome:",nome)
+print("CPF:",cpf)
+print("Endereço:",endereço)
+print("Idade:",idade, "anos")
+print("Altura:",altura, "metros")
+print('Telefone:', telefone)
+
+#aspas dupla " parece que dá espaço no print, coisa que não acontece na simples
---
+++ readme.md
@@ -0,0 +1,23 @@
+#git config --global user.name "username"
+#git config --global "@email.com"
+#git config --list (para confirmar se está certo)
+
+---
+>trocar nome da Branch: git branch -M "ex:main"
+git add (nome do arquivo editado)
+git status (para conferir o que está na lista para o push)
+git commit -m "título do commit "
+
+
+
+
+
+
+#\n > pula linha
+#\t > botão de espaço TAB 
+
+#type(exemplo) > para ver qual o tipo da variável
+#str : string (palavra)
+#int : num. inteiro
+#float : num. quebrado ex: 1.78 
+#variáveis sempre começam com letras minúsculas, ex: nome ou nome_complemento
