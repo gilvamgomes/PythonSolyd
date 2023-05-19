fed98b49b879e382182e1dae7ef4ee01af74706f
gilvamgomes <ggomes.fhb@gmail.com>
2023-05-19 13:59:19 -0300

    sexta aula, sem exec.

---
+++ aula-6-list-dict-tuple-set.py
@@ -0,0 +1,35 @@
+#list, dict, tuple, set
+
+minha_lista = ['Gui', 'João'] []
+#lista (list); usa quando precisa de algo ordenado
+minha_tupla = ('Gui', 'João') ()
+#tupla (tuple) imutável, num. limitado; usa quando term num. definido de coisas
+#Pode ser alterada se for toda alterada; 
+meu_dicionario = {'nome': 'Guilherme', 'idade': 27} {} 
+#dict; 
+meu_conjunto = {'Gui', 'João'} set()
+# set; não tem itens repetidos; não é ordenado 
+#Tem busca instantânea, 
+
+#como pesquisar nome ou objetos na lista e tupla:
+#if nome in minha_lista | if 'Gui' in minha_lista:
+#   print(nome)         |   print('Gui está aqui')    
+# 
+#como pesquisar no dicionário:
+#print (meu_dicionario['nome']) r: Guilherme
+#
+#if 'Guilherme' in meu_dicionario.alue():
+#   print('Guilherme esta no dicionario')
+
+# como adicionar no dicionário:
+# meu_dicionario['telefone'] = '61612513131'
+
+# como editar algo dentro do meu_dicionario
+# meu_dicionario['nome'] = 'joao' (trocando pelo pelo Gui, anterior)
+
+# busca rapida no conjunto
+# if 'Gui' in meu_conjunto:
+#     print('foi encontrrado no conjunto')
+
+
+
